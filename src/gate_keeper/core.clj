04e591cac55ce9189715135d4d2374157c63a5c9
Aaron Probus and Zahid Jethani <pair+aaron+zahid@fundingcircle.com>
2015-02-23 09:54:32 -0800

    Adds configurable request handler

--- src/gate_keeper/core.clj
+++ src/gate_keeper/core.clj
@@ -1,6 +1,16 @@
 (ns gate-keeper.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def find-first (comp first filter))
+
+(def not-authenticated-response
+  {:status 401
+   :body "Not authenticated"})
+
+(defmacro def-gate-keeper [fn-sym opts]
+  (let [{:keys [authenticators proxy-fn]} opts]
+    `(defn ~fn-sym [request#]
+       (if-let [authenticator# (find-first #(.handle-request? % request#) ~authenticators)]
+         (if (.authenticated? authenticator# request#)
+           (~proxy-fn request#)
+           not-authenticated-response)
+         not-authenticated-response))))
