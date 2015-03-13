26cf8a89dc4dbe7325642508115ff501f9f42425
Aaron Probus <pair+aaron@fundingcircle.com>
2015-03-13 11:03:24 -0700

    Allows custom handlers when request could not be authenticated/forwarded

--- src/gatekeeper/core.clj
+++ src/gatekeeper/core.clj
@@ -10,13 +10,21 @@
   {:status 401
    :body (json/write-str {:message "Not authenticated"})})
 
+(defn- handle-not-found [request]
+  not-found-response)
+
+(defn- handle-not-authenticated [request]
+  not-authenticated-response)
+
 (defmacro def-gatekeeper [fn-sym opts]
-  (let [{:keys [authenticators proxy-fn]} opts]
+  (let [{:keys [authenticators proxy-fn]} opts
+        not-found-fn (get opts :handle-not-found handle-not-found)
+        not-authenticated-fn (get opts :handle-not-authenticated handle-not-authenticated)]
     `(defn ~fn-sym [request#]
        (if-let [authenticator# (find-first #(.handle-request? % request#) ~authenticators)]
          (if-let [authenticated-request# (.authenticate authenticator# request#)]
            (if-let [response# (~proxy-fn authenticated-request#)]
              response#
-             not-found-response)
-           not-authenticated-response)
-         not-authenticated-response))))
+             (~not-found-fn request#))
+           (~not-authenticated-fn request#))
+         (~not-authenticated-fn request#)))))
