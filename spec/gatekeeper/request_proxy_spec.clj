5ecd084dccdf4131b26c3819806be0f321cad5da
Aaron Probus <pair+aaron@fundingcircle.com>
2015-03-15 15:44:00 -0700

    Converts request proxy to a middleware/handler

--- spec/gatekeeper/request_proxy_spec.clj
+++ spec/gatekeeper/request_proxy_spec.clj
@@ -10,16 +10,24 @@
 (context "auth-proxy.request-proxy"
   (describe "proxy-request-by-server"
     (context "when forwarding server is not found"
-      (it "is falsy when forwarding server not found"
-        (should-not
-          (proxy-request-by-server {} {:server-name "here" :request-method :get}))))
+      (with proxy-with-handler-fn (proxy-request-by-server (constantly {:status 200}) {}))
+      (with proxy-fn (proxy-request-by-server {}))
+
+      (it "is not found when no optional handler supplied"
+        (should= 404
+          (:status (@proxy-fn {:server-name "here" :request-method :get}))))
+
+      (it "responds with the optional handler when present"
+        (should= 200
+          (:status (@proxy-with-handler-fn {:server-name "here" :request-method :get})))))
 
     (context "when forwarding server is found"
+      (with proxy-fn (proxy-request-by-server {"hello" "http://hello.com"}))
       (with response
         (with-fake-routes-in-isolation hello-request
-          (proxy-request-by-server {"hello" "http://hello.com"} {:server-name "hello"
-                                                                 :request-method :get
-                                                                 :uri "/gatekeeper"})))
+          (@proxy-fn {:server-name "hello"
+                      :request-method :get
+                      :uri "/gatekeeper"})))
       (it "forwards the response status"
         (should= 200
                  (:status @response)))
--- src/gatekeeper/request_proxy.clj
+++ src/gatekeeper/request_proxy.clj
@@ -3,10 +3,16 @@
             [clj-http.client :as client]
             [clojure.data.json :as json]))
 
+(def ^:private not-found-response {:status 404
+                                   :body (json/write-str {:message "Not found"})})
+
 (defn- build-url [host path]
   (.toString (java.net.URL. (java.net.URL. host) path)))
 
-(defn proxy-request-by-server [server-to-host request]
+(defn- handle-not-found [request]
+  not-found-response)
+
+(defn- proxy-request-by-server* [handler server-to-host request]
   (let [domain (:server-name request)
         host (get server-to-host domain)]
     (if host
@@ -18,4 +24,10 @@
                                     :throw-exceptions false
                                     :as :stream})
                    [:status :body])
-      nil)))
+      (handler request))))
+
+(defn proxy-request-by-server
+  ([server-to-host]
+   (proxy-request-by-server handle-not-found server-to-host))
+  ([handler server-to-host]
+   (partial proxy-request-by-server* handler server-to-host)))
