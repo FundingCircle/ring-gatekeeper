4766a56c3ebad0fa251f75ee9d39cbd822214fbf
Robert Crim <rob@servermilk.com>
2015-06-15 22:19:44 -0700

    Provide an in-memory cache store

---
+++ src/ring_gatekeeper/cache/memory.clj
@@ -0,0 +1,16 @@
+(ns ring-gatekeeper.cache.memory
+  (:refer-clojure :exclude [get set])
+  (:require [clojure.core.cache :as c]
+            [ring-gatekeeper.cache.core :as core]))
+
+(deftype Memory [ttl-cache]
+  core/Cache
+
+  (get [this key]
+      (c/lookup @ttl-cache key))
+
+  (set [this key value]
+    (swap! ttl-cache c/miss key value)))
+
+(defn new-cache [ttl]
+  (Memory. (atom (c/ttl-cache-factory {} :ttl ttl))))
