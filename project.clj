abf23332a97c69b2dcd18ec7bdeae90e64602465
Francisco Viramontes <kidpollo@gmail.com>
2018-01-12 15:37:41 -0800

    0.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fundingcircle/ring-gatekeeper "0.3.0"
+(defproject fundingcircle/ring-gatekeeper "0.3.1-SNAPSHOT"
   :description "Authentication middleware"
   :url "https://github.com/FundingCircle/ring-gatekeeper"
   :license {:name "BSD 3-clause"
